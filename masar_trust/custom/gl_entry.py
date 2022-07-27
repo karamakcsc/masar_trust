5aa8deda33520ec7493ca9f437b5d16c413ca466
karamakcsc <karamakcsc@hotmail.com>
2022-07-27 09:22:53 +0000

    active set warehouse

--- masar_trust/custom/gl_entry.py
+++ masar_trust/custom/gl_entry.py
@@ -1,11 +1,11 @@
-def exclude_payroll_general_ledger(user):
-
-        if not user: user = frappe.session.user
-
-        if user != 'Administrator' and 'Admin' not in frappe.get_roles():
-
-            return """(name ='Iraq Services' in (select name from `tabSupplier Group`
-
-                 where account = (select 21110000 - Creditors - TRUST from `tabCompany`
-
-                            where name = company)))"""
+# def exclude_payroll_general_ledger(user):
+#
+#         if not user: user = frappe.session.user
+#
+#         if user != 'Administrator' and 'Admin' not in frappe.get_roles():
+#
+#             return """(name ='Iraq Services' in (select name from `tabSupplier Group`
+#
+#                  where account = (select 21110000 - Creditors - TRUST from `tabCompany`
+#
+#                             where name = company)))"""
