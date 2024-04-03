ffdd2929ba4092a6925d99238c7d26ad0377ff03
karamakcsc <karamakcsc@hotmail.com>
2024-04-03 13:34:49 +0300

    PO

--- masar_trust/custom/purchase_order/purchase_order.js
+++ masar_trust/custom/purchase_order/purchase_order.js
@@ -9,29 +9,4 @@ frappe.ui.form.on("Purchase Order","refresh", function(frm) {
      frm.toggle_display("more_info", false);
 });
 
-
-
-// frappe.ui.form.on("Purchase Order", "refresh", function(frm) {
-//        frm.set_query("item_code", "items", function(doc, cdt, cdn) {
-//       	var d = locals[cdt][cdn];
-//       	return {
-//       		query: "masar_trust.custom.purchase_order.purchase_order.get_item_supplier",
-//       		filters:{
-//       				'supplier': doc.supplier
-//       		}
-//       	}
-//       })
-// });
-
-
-
-// frappe.ui.form.on("Purchase Order", {
-//   supplier: function(frm) {
-//     if(frm.doc.items.length > 0){
-//       frm.clear_table("items");
-//       frm.refresh_fields();
-//     }
-//   }
-// });
-
-
+ 
--- masar_trust/custom/purchase_order/purchase_order.py
+++ masar_trust/custom/purchase_order/purchase_order.py
@@ -1,22 +1,12 @@
 import frappe
-# import erpnext
-# import math
-# from frappe.utils import flt, get_datetime, getdate, date_diff, cint, nowdate, get_link_to_form, time_diff_in_hours
-# from frappe import throw, msgprint, _
 
-# @frappe.whitelist(allow_guest=True)
-# def get_item_supplier(doctype, txt, searchfield, start, page_len, filters):
-#     #return frappe.db.sql("""Select item_code from `tabItem` where default_supplier = %s""", 'Supplier001')
-#     return frappe.db.sql("""Select item_code  , description from `tabItem` ti
-# 							inner join `tabItem Supplier` tis
-#     						on tis.parent = ti.name
-# 							Where tis.supplier = %s""", ((filters.get("supplier"))))
 
 def on_submit(self , name):
-    check_item_code_supplier(self)
+    check_item_code_supplier(self.name , self.supplier)
+
 
 @frappe.whitelist()
-def check_item_code_supplier(self):
+def check_item_code_supplier(name , supplier):
     supplier_items = frappe.db.sql("""
         SELECT 
             ti.item_code, 
@@ -25,7 +15,7 @@ def check_item_code_supplier(self):
         FROM `tabItem` ti
         INNER JOIN `tabItem Supplier` tis ON tis.parent = ti.name
         WHERE tis.supplier = %s
-    """, (self.supplier), as_dict=True)
+    """, (supplier), as_dict=True)
 
     supplier_item_codes = {item['item_code'] for item in supplier_items}
 
@@ -35,9 +25,9 @@ def check_item_code_supplier(self):
         FROM `tabPurchase Order` tpo 
         INNER JOIN `tabPurchase Order Item` tpoi ON tpoi.parent = tpo.name 
         WHERE tpo.name = %s
-    """, (self.name), as_dict=True)
+    """, (name), as_dict=True)
     msg_print = False
-    error_message = f"Error: The following items are not available for the specified supplier ({self.supplier}):"
+    error_message = f"Error: The following items are not available for the specified supplier ({supplier}):"
     for items in po_items:
            item_code = items.get('item_code')
            idx = items.get('idx')
