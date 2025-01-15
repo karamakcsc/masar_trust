0c0f1d4f13e10fdb9351eddcdb494950f2bd532d
karamakcsc <karamakcsc@hotmail.com>
2025-01-15 11:34:23 +0300

    update

--- masar_trust/masar_trust/report/sales_invoice_cost/sales_invoice_cost.py
+++ masar_trust/masar_trust/report/sales_invoice_cost/sales_invoice_cost.py
@@ -45,7 +45,7 @@ def get_data(filters):
 	#                                                  Where tsii.docstatus =1  AND (tsi.posting_date BETWEEN '{_from}' AND '{to}')
 	# 						 {conditions}) as t;""")
 
-	data = frappe.db.sql(f""" Select tsii.parent as `Sales Invoice`, tsi.posting_date as `Posting Date`, tsi.customer_name as `Customer Name`,tsii.item_code as `Item Code` ,tsii.item_name as `Item Name`,
+	data = frappe.db.sql(f""" Select tsii.parent as `Sales Invoice`, tsi.posting_date as `Posting Date`, tsi.customer as `Customer ID`,tc.customer_group AS `Customer Group`,tsi.customer_sub AS `Customer Sub`, tsii.item_code as `Item Code` ,tsii.item_name as `Item Name`,
 								tsii.description as `Item Description`, tsii.item_group as `Item Group` ,tdvn.parent as `Delivery Note`,tsii.qty as `Invoice QTY`,tdvn.qty as `Delivery Note QTY`, tsii.rate as `Sales Rate`, tsii.net_rate as `Net Sales Rate Per Unit`,
 								Round(IF(IF(IF(tdvn.stock_value_difference IS NULL,tsle.stock_value_difference /tsle.actual_qty ,tdvn.stock_value_difference/tdvn.qty)IS NULL,0,IF(tdvn.stock_value_difference IS NULL,tsle.stock_value_difference/tsle.actual_qty ,
 								tdvn.stock_value_difference/tdvn.qty)) >=0, IF(IF(tdvn.stock_value_difference IS NULL,tsle.stock_value_difference /tsle.actual_qty ,tdvn.stock_value_difference/tdvn.qty)IS NULL,0,IF(tdvn.stock_value_difference IS NULL,tsle.stock_value_difference/tsle.actual_qty ,
@@ -63,12 +63,15 @@ def get_data(filters):
 								tdvn.stock_value_difference/tdvn.qty)), -1 * (IF(IF(tdvn.stock_value_difference IS NULL,tsle.stock_value_difference /tsle.actual_qty ,tdvn.stock_value_difference/tdvn.qty)IS NULL,0,IF(tdvn.stock_value_difference IS NULL,tsle.stock_value_difference/tsle.actual_qty ,
 								tdvn.stock_value_difference/tdvn.qty)))) *  tsii.qty )) /tsii.net_amount) * 100)  as `Gross Profit Percentage`,
 								tsii.discount_amount as `Discount Per Unit`,(tsii.discount_amount * tsii.qty) as `Total Discount Amount`,(tsii.amount - tsii.net_amount) as `Additional Discount Amount`
+								, tst.sales_person AS `Sales Person`
 								From `tabSales Invoice Item` tsii
 								left JOIN `tabSales Invoice` tsi on  tsii.parent = tsi.name
 								Left Join `tabStock Ledger Entry` tsle on tsii.name = tsle.voucher_detail_no and tsii.parent = tsle.voucher_no
 								left join (select tdni.parent,tdni.name,(tsle2.stock_value_difference / tsle2.actual_qty) as UnitCost,tsle2.stock_value_difference,qty
 								from `tabDelivery Note Item` tdni
 								inner join `tabStock Ledger Entry` tsle2 on tdni.name = tsle2.voucher_detail_no and tdni.parent = tsle2.voucher_no) as tdvn on tsii.delivery_note =tdvn.parent and tsii.dn_detail = tdvn.name
+								LEFT JOIN `tabCustomer` tc ON tsi.customer = tc.name
+								LEFT JOIN `tabSales Team` tst ON tst.parent = tsi.name
 								Where tsii.docstatus =1 AND
 							(tsi.posting_date BETWEEN '{_from}' AND '{to}')
 							 {conditions};""")
@@ -84,7 +87,9 @@ def get_columns():
 	return [
 	   "Sales Invoice: Link/Sales Invoice:200",
 	   "Posting Date: Date/ Posting Date:120",
-	   "Customer Name: Link/Customer:120",
+	   "Customer ID: Link/Customer:120",
+	   "Customer Group: Link/Customer Group:120", 
+       "Customer Sub: Link/Customer Sub:120",
 	   "Item Code: Link/Item:200",
 	   "Item Name: Link/Item:200",
 	   "Item Description: Data:200",
@@ -105,6 +110,7 @@ def get_columns():
 	   "Gross Profit Percentage: Percent:200",
 	   "Discount Per Unit: Data:200",
 	   "Total Discount Amount: Data:200",
-	   "Additional Discount Amount: Data:200"
+	   "Additional Discount Amount: Data:200", 
+	   "Sales Person: Link/Sales Person:120"
 
 	]
